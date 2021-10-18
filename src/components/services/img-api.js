49b31bc2d8cf2ed63fa2a30e3a55a9eeab2f01b4
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-10-18 20:18:13 +0300

    Добавляет изминения

--- src/components/services/img-api.js
+++ src/components/services/img-api.js
@@ -8,19 +8,3 @@ function fetchApi(nextQuery, nextPage) {
 const apiFirst = { fetchApi };
 
 export default apiFirst;
-
-// function fetchPokemon(name) {
-//   return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-
-//     return Promise.reject(new Error(`Нет покемона с именем ${name}`));
-//   });
-// }
-
-// const api = {
-//   fetchPokemon,
-// };
-
-// export default api;
